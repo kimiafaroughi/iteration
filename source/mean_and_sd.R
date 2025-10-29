54c9b98e774d426c28ae2d492455715d88645129
kimiafaroughi <ksf2129@cumc.columbia.edu>
2025-10-29 18:53:47 -0400

    catching up

---
+++ source/mean_and_sd.R
@@ -0,0 +1,29 @@
+#' mean_and_sd
+#' 
+#' Takes a numeric input vector and computes the mean and sd
+#' 
+#' @param x the input vector (must be numeric with 5 or more elements)
+#' 
+#' @returns a dataframe containing the mean and sd of x
+#' 
+mean_and_sd = function(x) {
+  
+  if (!is.numeric(x)) {
+    stop("The input x should be numeric")
+  }
+  
+  if (length(x) < 5) {
+    stop("Only compute mean and sd when the input has 5 or more numbers")
+  }
+  
+  mean_x = mean(x, na.rm = TRUE)
+  sd_x = sd(x, na.rm = TRUE)
+  
+  tibble(
+    mean = mean_x,
+    sd = sd_x
+  )
+  
+}
+
+mean_and_sd(x = x_vec)
